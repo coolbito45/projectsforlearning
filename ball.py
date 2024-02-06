90dda975b9b082a1033a771e9c6e4b0470a7e78c
bito <b.kondrak45@gmail.com>
2024-02-05 21:20:59 -0700

    i changed it from 26 to 10

--- ball.py
+++ ball.py
@@ -1,8 +1,8 @@
 import time
 while True:
-    for i in range(26):
+    for i in range(10):
         print(" "*i + "0")
         time.sleep(0.1)
-    for i in reversed(range(26)):
+    for i in reversed(range(10)):
         print(" "*i + "0")
         time.sleep(0.1)
