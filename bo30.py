7c5b7c6c2a6548754d99f31dcdc1f4ee834b432c
bito <b.kondrak45@gmail.com>
2024-02-17 17:12:59 -0700

    uploading mo10bo3 analyzer

---
+++ bo30.py
@@ -0,0 +1,35 @@
+import csv
+
+def minute(value):
+    index_colon = value.find(":")
+    if index_colon != -1:
+        minutes = float(value[:index_colon])
+        seconds = float(value[index_colon+1:])
+        return seconds + minutes*60
+    else:
+        return value
+    
+
+solves = []
+with open("csTimerExport_20240217_162331.csv") as csvfile:
+
+    reader = csv.reader(csvfile, delimiter=';')
+    for row in reader:
+        #print(row[1])
+        if row[1] != "Time":
+            solves.append(row[1])
+
+#print(solves)
+for i, solve in enumerate(solves):
+    if solve.startswith("DNF"):
+       solves[i] = float("inf")
+    else:
+        solves[i] = float(minute(solve))
+#print(solves)
+
+best_solves = []
+
+for i in range(10):
+    best_solves.append(min(solves[i*3], solves[i*3 + 1], solves[i*3 + 2]))
+
+print(sum(best_solves) / 10)
